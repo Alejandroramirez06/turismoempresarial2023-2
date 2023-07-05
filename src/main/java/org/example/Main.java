a5cdd71c8ad1262ffeccd556a1ca0f62e2ac5b09
Ivett Barrios <ivettale5@gmail.com>
2023-07-05 11:54:36 -0500

    se crea ejemplo de modificadores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.modelos.Usuario;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer edad=19;
+        Universidad objetoUniversidad=new Universidad("Alejandro");
+        Usuario objetoUsuario=new Usuario(25,"Alejandro","123",4,"alejo@gmail.com");
+        System.out.println(objetoUniversidad);
+        System.out.println(objetoUsuario);
     }
 }
